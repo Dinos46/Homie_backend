e1408de0ec646bf6c189503b0c0083dff40edeae
Din <dinos4620@gmail.com>
2021-06-08 12:05:11 +0200

    fff

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -18,7 +18,7 @@ async function signup(req, res) {
     try {
         const { username, password, fullname, imgUrl} = req.body
         // Never log passwords
-        // logger.debug(fullname + ', ' + username + ', ' + password)
+        logger.debug(`${username}, ${password}, ${fullname}, ${imgUrl}`)
         const account = await authService.signup(username, password, fullname, imgUrl)
         logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
         const user = await authService.login(username, password)
